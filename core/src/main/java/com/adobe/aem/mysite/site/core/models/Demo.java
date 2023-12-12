1926e9cb304aea586f722412139eac380214b096
GopalR <gopalraikindi5@gmail.com>
2023-12-12 12:03:55 +0530

    Testing

---
+++ core/src/main/java/com/adobe/aem/mysite/site/core/models/Demo.java
@@ -0,0 +1,10 @@
+package com.adobe.aem.mysite.site.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables=Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class Demo {
+
+}
